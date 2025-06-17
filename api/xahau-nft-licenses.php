a5461c3d9626b79cac28804154b41b52c093d1a1
h20crypto <h20crypto18@gmail.com>
2025-06-17 13:29:31 -0500

    Update xahau-nft-licenses.php

--- api/xahau-nft-licenses.php
+++ api/xahau-nft-licenses.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once 'crypto-rates-optimized.php'; // Include our optimized Dhali rates
+require_once 'crypto-rates-optimized.php';
 
 class XahauNFTLicenseManager {
     private $license_file = 'data/licenses.json';
